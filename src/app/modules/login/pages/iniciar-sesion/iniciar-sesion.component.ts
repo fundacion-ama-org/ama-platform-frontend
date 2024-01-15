41b893bf9c9cc8ddcb48e4242f2718ad6f5ea84d
Jorge Molina <jm0241895@gmail.com>
2024-01-15 00:45:55 -0500

    consumo del servicio del login

--- src/app/modules/login/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/modules/login/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,53 +1,71 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
+import { Router } from '@angular/router';  // Asegúrate de importar el Router
 import { LoginService } from '../../services/login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
-  styleUrl: './iniciar-sesion.component.css'
+  styleUrls: ['./iniciar-sesion.component.css']
 })
 export class IniciarSesionComponent {
+
   loginForm: FormGroup;
 
-  constructor(
-    private router: Router,
-    private authService: LoginService,
-    private formBuilder: FormBuilder,
-  ) {
-    this.loginForm = this.formBuilder.group({
+  constructor(private loginService: LoginService, private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
   onSubmit() {
-    console.log(this.loginForm.valid)
-    if (this.loginForm.valid) {
-      const username = this.loginForm.get('username')?.value;
-      const password = this.loginForm.get('password')?.value;
+    const usernameControl = this.loginForm.get('username');
+    const passwordControl = this.loginForm.get('password');
 
-      // Utiliza el servicio de autenticación para verificar las credenciales
-      const isAuthenticated = this.authService.login(username, password);
+    if (usernameControl && passwordControl) {
+      const identification = usernameControl.value;
+      const password = passwordControl.value;
 
-      if (isAuthenticated) {
-        // Si las credenciales son válidas, redirige a la ruta deseada
-        this.router.navigateByUrl('/admin');
+      this.loginService.signIn(identification, password).subscribe(response => {
+        console.log(response);
+        this.router.navigate(['/listar']);
+      }, error => {
+        console.error(error);
 
-      } else {
-        // Si las credenciales son inválidas, puedes mostrar un mensaje o realizar otra acción
-        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
-      }
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Usuario o contraseña incorrecta!',
+        });
+      });
+    } else {
+      console.error('Error al acceder a los controles del formulario');
     }
   }
 
-  onClickLogin() {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Usuario o contraseña incorrecta!",
-    });
-  }
+  // onSubmit() {
+  //   if (this.loginForm.valid) {
+  //     const username = this.loginForm.get('username')?.value;
+  //     const password = this.loginForm.get('password')?.value;
+
+  //     // Llamada al servicio de autenticación
+  //     this.loginService.signIn(username, password).subscribe(
+  //       (response: any) => {
+  //         if (response.success) {
+  //           // Si las credenciales son válidas, redirige a la ruta deseada
+  //           this.router.navigate(['/brigada']);
+  //         } else {
+  //           // Si las credenciales son inválidas, puedes mostrar un mensaje o realizar otra acción
+  //           alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+  //         }
+  //       },
+  //       (error: any) => {
+  //         console.error('Error al realizar la solicitud:', error);
+  //         // Puedes mostrar un mensaje de error o realizar otra acción según tus necesidades
+  //       }
+  //     );
+  //   }
+  // }
 }
--- src/app/modules/login/services/login.service.ts
+++ src/app/modules/login/services/login.service.ts
@@ -1,14 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): boolean {
-    // Lógica de autenticación simulada con credenciales estáticas
-    return username === 'admin' && password === 'admin';
+  signIn(identification: string, password: string): Observable<any> {
+    const url = 'https://platform-ama-backend.azurewebsites.net/api/Auth/signin';
+    const body = {
+      identification,
+      password
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(url, JSON.stringify(body), { headers })
+      .pipe(
+        map(resp => resp),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred';
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    return throwError(errorMessage);
   }
 }
