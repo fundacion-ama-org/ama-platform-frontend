41eae96337a6e2c9bf601d06b711abeeb6791c6d
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2024-01-18 00:36:28 -0500

    feat:(Directives): Implementacion de Directivas

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -7,9 +7,8 @@ export class SharedService {
   public soloLetras = /^[A-Za-zÁÉÍÓÍáéíóú]+$/
   public onlyNumerosValores = /^[1-9][0-9]*$/;
   public onlyNumeros = /^[0-9]+$/
-  public onlyLetrasEspacios = /^[ÃÃ‰ÃÃ“ÃÃšÃ¡Ã©Ã­Ã³Ãº0A-Za-z ]+$/
+  public onlyLetrasEspacios = /^[ÁÉÍÓÍÚáéíóú0A-Za-z ]+$/
   public soloNumerosLetras = /^[0-9A-Za-z]+$/
-  public onlyLetrasNumeroEspacios = /^[ÃÃ‰ÃÃ“ÃÃšÃ¡Ã©Ã­Ã³Ãº0-9A-Za-z ]+$/
+  public onlyLetrasNumeroEspacios = /^[ÁÉÍÓÍÚáéíóú0-9A-Za-z ]+$/
   public onlyLetras = /^[A-Za-z]+$/
-
 }
