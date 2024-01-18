8209917636534f8361b71109d741e87c3c5a73f8
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2024-01-18 00:25:25 -0500

    shared Service

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -5,4 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class SharedService {
   public soloLetras = /^[A-Za-zÁÉÍÓÍáéíóú]+$/
+  public onlyNumerosValores = /^[1-9][0-9]*$/;
+  public onlyNumeros = /^[0-9]+$/
+  public onlyLetrasEspacios = /^[ÃÃ‰ÃÃ“ÃÃšÃ¡Ã©Ã­Ã³Ãº0A-Za-z ]+$/
+  public soloNumerosLetras = /^[0-9A-Za-z]+$/
+  public onlyLetrasNumeroEspacios = /^[ÃÃ‰ÃÃ“ÃÃšÃ¡Ã©Ã­Ã³Ãº0-9A-Za-z ]+$/
+  public onlyLetras = /^[A-Za-z]+$/
+
 }
